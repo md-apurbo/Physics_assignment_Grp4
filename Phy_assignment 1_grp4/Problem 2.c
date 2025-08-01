8125d96494ba3fc3835121cee67e77349628d341
irienpriya <irienaktherpriya@gmail.com>
2025-08-01 15:34:46 +0600

    Update Problem 2.c

--- Phy_assignment 1_grp4/Problem 2.c
+++ Phy_assignment 1_grp4/Problem 2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    double lambda1 = 530; 
+    double theta1_deg = 65.0; 
+    int m1 = 3;
+
+    double lambda2 = 700;
+    int m2 = 2;
+
+    double theta1_rad = theta1_deg * M_PI / 180.0;
+    double d = (m1 * lambda1) / sin(theta1_rad);
+
+    double sin_theta2 = (m2 * lambda2) / d;
