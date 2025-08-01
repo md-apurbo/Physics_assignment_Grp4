1ba47715eeca734180f1df6769f63ccd5fb17a03
eyazurhusenkhan <piyashbd25@gmail.com>
2025-08-01 15:56:50 +0600

    Update Problem 1.c

--- Phy_assignment 1_grp4/Problem 1.c
+++ Phy_assignment 1_grp4/Problem 1.c
@@ -18,5 +18,23 @@ int main() {
 
     printf("Wavelength: %.2f nm\n", wavelength);
 
-    
+    if (wavelength >= 380 && wavelength < 450)
+        printf("Color: violet\n");
+    else if (wavelength >= 450 && wavelength < 485)
+        printf("Color: blue\n");
+    else if (wavelength >= 485 && wavelength < 500)
+        printf("Color: cyan\n");
+    else if (wavelength >= 500 && wavelength < 565)
+        printf("Color: green\n");
+    else if (wavelength >= 565 && wavelength < 590)
+        printf("Color: yellow\n");
+    else if (wavelength >= 590 && wavelength < 625)
+        printf("Color: orange\n");
+    else if (wavelength >= 625 && wavelength <= 750)
+        printf("Color: red\n");
+    else
+        printf("Color: not in visible range\n");
+
+    return 0;
+}
 
